dae72a98de2b790a5032af35893937e5b05c970d
Justin Martin <jaming@protonmail.com>
2022-09-20 00:53:31 -0400

    clamp

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn convert_tor_private(mnem: Mnemonic) -> Vec<u8> {
     let mut result = hasher.finalize();
     // clamp key for ed25519 spec
     // https://gitlab.torproject.org/dgoulet/torspec/blob/master/rend-spec-v3.txt#L2293
+    // TODO investigate clamping differences in tor with ref10 vs donna
     result[0] &= 248;
     result[31] &= 63; // 127
     result[31] |= 64;
@@ -426,6 +427,10 @@ fn convert_pgp_mnem(words: &str, lang: Language, time: SystemTime, duration: Dur
     let hash = Sha512::digest(ed_priv_key.to_bytes());
     let mut output = [0u8; 32];
     output.copy_from_slice(&hash[..32]);
+    // clamp
+    output[0] &= 248;
+    output[31] &= 127;
+    output[31] |= 64;
     let x25519_secret = x25519_dalek::StaticSecret::from(output);
     // import encryption secret subkey signed
     let mut aes_key: Key<_, SubordinateRole> = Key::from(Key4::import_secret_cv25519(&x25519_secret.to_bytes(), None, None, time).expect("Failed to import ecdh"));
